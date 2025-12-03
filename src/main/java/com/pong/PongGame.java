add441e27e5b2500cb10f3e5d0cae66aec6ff086
idress12three-ai <idress12three@gmail.com>
2025-12-03 15:30:16 +0000

    f

--- src/main/java/com/pong/PongGame.java
+++ src/main/java/com/pong/PongGame.java
@@ -1,3 +1,7 @@
+//idress Ahmadzai 12/2/25 
+// this project allows you to play pong against an ai
+
+
 package com.pong;
 
 import javax.swing.*;
@@ -32,7 +36,7 @@ public class PongGame extends JPanel implements MouseMotionListener {
         ball = new Ball(200, 200, 10, 3, Color.RED, 10);
         slow = new SlowDown(200,150,100,100);
         playerPaddle= new Paddle(10,240,50,9,Color.WHITE);
-        fast= new Speedup(200,400,100,100);
+        fast= new Speedup(200,300,100,100);
         //create any other objects necessary to play the game.
 
     }
@@ -72,21 +76,33 @@ public class PongGame extends JPanel implements MouseMotionListener {
     // postcondition: one frame of the game is "played"
     public void gameLogic() {
         //add commands here to make the game play propperly
+        
+        //makes the ball move
         ball.moveBall();
         
+        //makes the ai follows the ball y coordinates
         aiPaddle.moveY(ball.getY());
+        
+        //makes the player follows the mouse cursor
         playerPaddle.moveY(userMouseY);
+        
+        //if the ball touches the top or bottom of the game it bounces off
         ball.bounceOffwalls(600, 0);
         
-        
+        //if the ball touches the ai paddle it bounces off
         if (aiPaddle.isTouching(ball)) {
            ball.reverseX();
+           ball.reverseY();
         }
         
+        //if the ball touches the playerpaddle it bounces off
         if (playerPaddle.isTouching(ball)) {
            ball.reverseX();
+           ball.reverseY();
+           
         }
         
+        // makes the ball speed up when it touches the speed up zone
         if (fast.isTouching(ball)){
             if (ball.getChangeX()<0){
                 ball.setChangeX(ball.getChangeX()-.5);
@@ -97,16 +113,18 @@ public class PongGame extends JPanel implements MouseMotionListener {
             
         }
         
-        if (fast.isTouching(ball)){
+        // makes the ball slow down when it touches the slow down zone
+        if (slow.isTouching(ball)){
             if (ball.getChangeX()<0){
-                ball.setChangeX(ball.getChangeX()- .5);
+                ball.setChangeX(ball.getChangeX()+ .5);
             }
             else{
-                ball.setChangeX(ball.getChangeX()+ .5);
+                ball.setChangeX(ball.getChangeX()- .5);
             }
             
         }
         
+        //if the ball goes of the screen to the left the ai gets a point
         if (ball.getX()<=0){
             aiScore++;
             ball.setChangeX(10);
@@ -115,6 +133,7 @@ public class PongGame extends JPanel implements MouseMotionListener {
             ball.sety(200);
         }
         
+        //if the ball goes of the screen to the right the player gets a point
         if (ball.getX()>=width){
             playerScore++;
             ball.setChangeX(10);
